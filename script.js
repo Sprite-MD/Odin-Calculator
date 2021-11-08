5bb998e75131f5a23fbab35b5526f9a136bc4a79
sprite <ponkaew.wasin@gmail.com>
2021-11-08 13:51:04 -0600

    added basic functions

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ function divide(a,b){
     return a/b;
 }
 
+function operate(operator, a, b){
+    switch (operator){
+        case '+': 
+            return add(a,b);
+        case '-':
+            return subtract(a,b);
+        case '*':
+            return multiply(a,b);
+        case '/':
+            if (b == 0){ return null}
+            else{
+            return divide(a, b);
+            }
+    }
+}
